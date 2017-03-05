4a9660910e28fa46a9fbb6ba89f7aaea29517d1c
Shaoor Munir <shaoormunir@outlook.com>
2017-03-05 18:35:57 +0500

    Completed quick sort algorithm with deterministic pivot value

--- QuickSort (deterministic).py
+++ QuickSort (deterministic).py
@@ -1,5 +1,19 @@
 
+def partition(A, s, pivot):
+    i = s-1
+    j = s
 
+    while i< pivot and j < pivot:
+        if(A[j] > A[pivot]):
+            j+=1
+        else:
+            i+=1
+            A[i], A[j] = A[j], A[i]
+            j += 1
+
+    pivot = i+1
+    A[pivot], A[j] = A[j], A[pivot]
+    return pivot
 
 
 def quick_sort(A, s, e):
@@ -7,3 +21,20 @@ def quick_sort(A, s, e):
     if s >= e:
         return A
 
+    pivot = e
+    print (pivot)
+
+    pivot = partition(A, s, pivot)
+
+    quick_sort(A, s, pivot-1)
+    quick_sort(A, pivot+1, e)
+
+    return
+
+A = [8,6,7,3,2,1,5,6,7,4]
+
+print(A)
+
+quick_sort(A, 0, len(A)-1)
+
+print(A)
